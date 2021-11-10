f037f4e1b6cbf24d5b894d415bc2e47eb64a0670
Daniel-Mdn <danielmedina012@gmail.com>
2021-11-09 22:48:19 -0300

    Cambios

--- Aplicacion/App.py
+++ Aplicacion/App.py
@@ -5,6 +5,7 @@ import flask
 from flask.globals import request
 from flask.wrappers import Request
 from flask_mysqldb import MySQL
+import forms, functions
 
 app = Flask(__name__)
 
@@ -36,30 +37,54 @@ def logout():
     session.pop('username')
     return redirect(url_for('Index'))
 
-@app.route('/register')
+@app.route('/register', methods = ['GET', 'POST'])
 def Register():
-    return render_template('register.html')
-
-@app.route('/register_add', methods = ['POST'])
-def register_add():
+    registerForm= forms.RegisterForm(request.form)
     datos=[];
-    if request.method == 'POST':
+    if request.method == 'POST' and registerForm.validate():
         datos.append(request.form['usuario'])
         datos.append(request.form['nombre'])
         datos.append(request.form['apellido'])
         datos.append(request.form['mail'])
-        datos.append(request.form['password'])
+        datos.append(request.form['contrasenia'])
+        datos.append(request.form['sexo'])
+        datos.append(request.form['fechaNacimiento'])
+        datos.append(request.form['altura'])
+        datos.append(request.form['peso'])
+        datos.append(request.form['cintura'])
+        datos.append(request.form['cuello'])
+        datos.append(request.form['caderas'])
         x= (i for i in datos if i.strip()=="")
-        lista = list(x)       
+        lista = list(x) 
         if (len(lista)!=0):
             flash('Complete todos los datos')
         else:
+            
             cur = mysql.connection.cursor()
-            cur.execute('INSERT INTO usuario (usuario, nombre, apellido, mail, contra) VALUES (%s, %s, %s, %s, %s)',
-            (datos[0], datos[1], datos[2], datos[3], datos[4]))
-            mysql.connection.commit()
-            flash('Registrado correctamente')
-    return redirect(url_for('register'))
+            cur.execute('SELECT usuario,mail FROM usuario WHERE usuario="{0}"'.format(datos[0]))
+            coincidenciasUsuario= cur.fetchall()
+            cur.execute('SELECT usuario,mail FROM usuario WHERE mail="{0}"'.format(datos[3]))
+            coincidenciasMail= cur.fetchall()
+            print(datos)
+            if (len(coincidenciasMail)==0 and len(coincidenciasUsuario)==0):
+                cur.execute('INSERT INTO usuario (usuario, nombre, apellido, mail, contra, sexo, fecha_nacimiento, altura, peso, cintura, cuello, caderas) VALUES ("{0}", "{1}", "{2}", "{3}", "{4}", "{5}", "{6}", {7}, {8}, {9}, {10}, {11})'.format
+                (datos[0], datos[1], datos[2], datos[3], datos[4], datos[5], datos[6], datos[7], datos[8], datos[9], datos[10], datos[11]))
+                mysql.connection.commit()
+                flash('Registrado correctamente')
+                return redirect(url_for('Index', success=True))
+            else:
+                if (len(coincidenciasUsuario)!=0):
+                    flash('El usuario ya esta registrado')
+                if (len(coincidenciasMail)!=0):
+                    flash('El mail ya esta registrado')
+                return redirect(url_for('Register'))
+
+    return render_template('register.html', form=registerForm)
+
+@app.route('/register_add', methods = ['POST'])
+def register_add():
+
+    return redirect(url_for('Index'))
 
 @app.route('/home', methods=['POST', 'GET'])
 def home():
@@ -67,23 +92,57 @@ def home():
         usuario= request.form['usuario']
         contrasenia= request.form['contrasenia']
         cur= mysql.connection.cursor()
-        cur.execute('SELECT u.usuario FROM usuario u WHERE (u.usuario= %s or u.mail=%s) AND u.contra=%s', (usuario, usuario, contrasenia))
+        cur.execute('SELECT u.id_usuario FROM usuario u WHERE (u.usuario= %s or u.mail=%s) AND u.contra=%s', (usuario, usuario, contrasenia))
         datosUsuario= cur.fetchall()
         if (len(datosUsuario)!=0):
+            session['username']= datosUsuario[0][0]
+        else:    
+            flash("Alguno de los datos es incorrecto")
+            return redirect(url_for('Index', success=False))
 
-            session['username']= datosUsuario[0]
-            return render_template('home.html')
-        return redirect(url_for('Index'))
+    if 'username' in session and request.method== 'GET':
+        id=session['username']
+        cur= mysql.connection.cursor()
+        cur.execute('SELECT r.id_rutina, e.id_ejercicio, e.nombre, re.peso, re.repeticiones, re.series, re.descanso FROM usuario_rutina ur JOIN rutina r ON r.id_rutina=ur.id_rutina JOIN rutina_ejercicio re ON r.id_rutina=re.id_rutina JOIN ejercicio e ON e.id_ejercicio=re.id_ejercicio WHERE ur.id_usuario={0}'.format(id))
+        datosEjercicio= cur.fetchall()
+        cur.execute('SELECT distinct(r.id_rutina), r.nombre FROM usuario_rutina ur JOIN rutina r ON r.id_rutina=ur.id_rutina JOIN rutina_ejercicio re ON r.id_rutina=re.id_rutina WHERE ur.id_usuario={0}'.format(id))
+        rutinas= cur.fetchall()
+        datosRutinas= list()
+        for rut in rutinas:
+            ejercicioRut=list()
+            ejercicioRut.append(rut[0])
+            ejercicioRut.append(rut[1])
+            for datosRut in datosEjercicio:
+                aux= list()
+                if rut[0] == datosRut[0]:
+                    aux= list([datosRut[1], datosRut[2], datosRut[3], datosRut[4], datosRut[5], datosRut[6]])
+                    ejercicioRut.append(aux)
+            datosRutinas.append(ejercicioRut)
+        rutina= request.args.get('rutina')
+        if rutina!=None:
+            cur= mysql.connection.cursor()
+            cur.execute('SELECT e.id_ejercicio, e.nombre, re.peso, re.repeticiones, re.series, re.descanso FROM usuario_rutina ur JOIN rutina r ON r.id_rutina=ur.id_rutina JOIN rutina_ejercicio re ON r.id_rutina=re.id_rutina JOIN ejercicio e ON e.id_ejercicio=re.id_ejercicio WHERE ur.id_usuario={0} AND r.id_rutina={1}'.format(id, rutina))
+            ejercicios= cur.fetchall()
+            cur.execute('SELECT r.nombre FROM rutina r WHERE r.id_rutina={0}'.format(rutina))
+            rutina= cur.fetchall()
+            rutina=rutina[0][0]
+            return render_template('home.html', datosRutinas=datosRutinas, rutina=rutina, ejercicios=ejercicios)
+        return render_template('home.html', datosRutinas=datosRutinas, rutina=rutina)
     else:
-        if 'username' in session:
-            return render_template('home.html')
-        else:
-            return redirect(url_for('Index'))
+        return redirect(url_for('Index'))
 
-@app.route('/home/user')
+@app.route('/home/user', methods=['POST', 'GET']) 
 def perfil():
-    if 'username' in session:
+    if request.method =='POST':
         return render_template('perfil.html')
+    if 'username' in session and request.method=='GET':
+        cur= mysql.connection.cursor()
+        cur.execute('SELECT usuario, nombre, apellido, mail, contra, sexo, fecha_nacimiento, altura, peso, cintura, cuello, caderas FROM usuario WHERE id_usuario={0}'.format(session['username']))
+        usuario= cur.fetchall()
+        usuario=usuario[0]
+        print(usuario)
+        imc=functions.IMC(usuario[8], usuario[7])
+        return render_template('perfil.html', usuario=usuario, imc=imc, evaluacion=functions.evaluarIMC(imc), pg=functions.porcentajeGrasa(usuario[5], usuario[9], usuario[10], usuario[7], usuario[11]) )
     else:
         return redirect(url_for('Index'))
 
@@ -107,10 +166,11 @@ def add_rutina():
         cur= mysql.connection.cursor()
         cur.execute('SELECT r.nombre FROM rutina r JOIN usuario_rutina ur ON r.id_rutina=ur.id_rutina JOIN usuario u ON u.id_usuario=ur.id_usuario WHERE r.nombre="{0}" and u.usuario = "{1}"'.format(rutina, session['username']))
         rutinaRepetida = cur.fetchall()
-
         cur= mysql.connection.cursor()
-        cur.execute('SELECT u.id_usuario FROM usuario u WHERE u.usuario= "{0}"'.format(session['username']))
+        print(session['username'])
+        cur.execute('SELECT id_usuario FROM usuario u WHERE id_usuario= "{0}"'.format(session['username']))
         idUsuario= cur.fetchall()
+        print(idUsuario)
         if (len(rutinaRepetida)==0):
             cur= mysql.connection.cursor()
             cur.execute('INSERT into rutina(fecha, nombre, estado) VALUES (CURDATE(), %s , "Creada")', [rutina])
@@ -121,7 +181,7 @@ def add_rutina():
             idrutina= idrutina[0]
             idrutina= idrutina[0]
             session['idrutina']= idrutina
-            cur.execute('INSERT into usuario_rutina(id_usuario, id_rutina, fecha_rutina) VALUES (%s, %s , CURDATE())', [idUsuario[0], idrutina])
+            cur.execute('INSERT into usuario_rutina(id_usuario, id_rutina, fecha_rutina) VALUES (%s, %s , CURDATE())', [idUsuario[0][0], idrutina])
             mysql.connection.commit()
             cur= mysql.connection.cursor()
             cur.execute('SELECT * FROM ejercicio')
@@ -184,7 +244,7 @@ def create_rutina():
         else:
             idrutina= session['idrutina']
             cur= mysql.connection.cursor()
-            cur.execute('SELECT u.id_usuario FROM usuario u WHERE u.usuario= "{0}"'.format(session['username']))
+            cur.execute('SELECT u.id_usuario FROM usuario u WHERE u.id_usuario= "{0}"'.format(session['username']))
             idUsuario= cur.fetchall()
             idUsuario= idUsuario[0]
             idUsuario= idUsuario[0]
@@ -202,25 +262,16 @@ def create_rutina():
 def modify_rutina():
     if 'username' in session:
         cur= mysql.connection.cursor()
-        cur.execute('SELECT r.id_rutina, r.nombre FROM rutina r JOIN usuario_rutina ur ON r.id_rutina=ur.id_rutina JOIN usuario u ON u.id_usuario=ur.id_usuario WHERE u.usuario= %s ', 
+        cur.execute('SELECT r.id_rutina, r.nombre FROM rutina r JOIN usuario_rutina ur ON r.id_rutina=ur.id_rutina WHERE ur.id_usuario= %s ', 
         [session['username']])
         rutinas=cur.fetchall()
         cur= mysql.connection.cursor()
-        cur.execute('SELECT r.id_rutina, e.id_ejercicio, e.nombre, re.series, re.repeticiones, re.peso, re.descanso FROM rutina r JOIN usuario_rutina ur ON r.id_rutina=ur.id_rutina JOIN usuario u ON u.id_usuario=ur.id_usuario JOIN rutina_ejercicio re ON r.id_rutina=re.id_rutina JOIN ejercicio e ON re.id_ejercicio=e.id_ejercicio WHERE u.usuario= %s ', 
+        cur.execute('SELECT r.id_rutina, e.id_ejercicio, e.nombre, re.series, re.repeticiones, re.peso, re.descanso FROM rutina r JOIN usuario_rutina ur ON r.id_rutina=ur.id_rutina JOIN rutina_ejercicio re ON r.id_rutina=re.id_rutina JOIN ejercicio e ON re.id_ejercicio=e.id_ejercicio WHERE ur.id_usuario= %s ', 
         [session['username']])
         rutinaEjercicios=cur.fetchall()
         cur= mysql.connection.cursor()
-        cur.execute('SELECT max(id_rutina) FROM rutina')
-        idrutina=cur.fetchall()
-        idrutina= idrutina[0]
-        idrutina= idrutina[0]
-        session['idrutina']= idrutina
-        cur= mysql.connection.cursor()
         cur.execute('SELECT * FROM ejercicio')
         datosEjer=cur.fetchall()
-        print(rutinaEjercicios)
-        #print(rutinas)
-        print("ingreso al template")
         return render_template('modify_rutina.html', rutinas=rutinas , ejercicios=datosEjer, rutinaEjer=rutinaEjercicios)
     else:
         return redirect(url_for('Index'))
@@ -230,19 +281,23 @@ def add_modification():
     if request.method == 'POST':
         cantEjerc= len(request.form.getlist('orden'))
         if cantEjerc>0 and request.form.get('botonSubmit')=="botonSubmit":
+            nombrerutina= request.form['nomb-rut']
+            idrutina=request.form['numb-rut']
             nombreEjer= request.form.getlist('nombre-ejer')
             series= request.form.getlist('series')
             repeticiones= request.form.getlist('repeticiones')
             peso= request.form.getlist('peso')
             descansos= request.form.getlist('descansos')
-            idrutina= session['idrutina']
             cur= mysql.connection.cursor()
-            cur.execute('SELECT fecha FROM rutina WHERE id_rutina= {0}'.format(idrutina)) 
+            cur.execute('UPDATE rutina r SET fecha=CURDATE() WHERE r.id_rutina={0}'.format(idrutina))
+            cur.execute('DELETE FROM rutina_ejercicio WHERE id_rutina={0}'.format(idrutina))
+            mysql.connection.commit()         
+            cur.execute('SELECT r.fecha FROM rutina r WHERE r.id_rutina={0}'.format(idrutina))
             fecharutina= cur.fetchall()
+            print(fecharutina)
             fecharutina=fecharutina[0]
             fecharutina=fecharutina[0]
             for i in range(cantEjerc):
-                cur= mysql.connection.cursor()
                 cur.execute('SELECT id_ejercicio FROM ejercicio WHERE nombre= %s' , [nombreEjer[i]]) 
                 idejercicio= cur.fetchall()
                 idejercicio=idejercicio[0]
@@ -251,24 +306,49 @@ def add_modification():
                 cur.execute('INSERT into rutina_ejercicio(fecha_rutina, id_rutina, id_ejercicio, descanso, repeticiones, peso, series) VALUES( %s, %s, %s, %s, %s, %s, %s )',
                 (fecharutina, idrutina, idejercicio, descansos[i], repeticiones[i], peso[i], series[i]))
                 mysql.connection.commit()
-            session.pop('idrutina')
-            flash('Creada correctamente')
+            flash('Modificada correctamente')
         else:
-            idrutina= session['idrutina']
-            cur= mysql.connection.cursor()
-            cur.execute('SELECT u.id_usuario FROM usuario u WHERE u.usuario= "{0}"'.format(session['username']))
-            idUsuario= cur.fetchall()
-            idUsuario= idUsuario[0]
-            idUsuario= idUsuario[0]
-            cur.execute('DELETE FROM usuario_rutina WHERE id_rutina = %s and id_usuario= %s' , [idrutina, idUsuario])
-            mysql.connection.commit()
-            cur.execute('DELETE FROM rutina WHERE id_rutina = %s', [idrutina])
-            mysql.connection.commit()
-            session.pop('idrutina')
-            flash('No se agrego ningun ejercicio')
-
+            flash('Tiene que agregar algun ejercicio a la rutina')
         return redirect(url_for('adm_rutinas'))
     return redirect(url_for('adm_rutinas'))
+
+@app.route('/home/administracion_rutinas/delete_rutina')
+def delete_rutina():
+    if 'username' in session:
+        cur= mysql.connection.cursor()
+        cur.execute('SELECT r.id_rutina, r.nombre FROM rutina r JOIN usuario_rutina ur ON r.id_rutina=ur.id_rutina WHERE ur.id_usuario= %s ', 
+        [session['username']])
+        rutinas=cur.fetchall()
+        cur= mysql.connection.cursor()
+        cur.execute('SELECT r.id_rutina, e.id_ejercicio, e.nombre, re.series, re.repeticiones, re.peso, re.descanso FROM rutina r JOIN usuario_rutina ur ON r.id_rutina=ur.id_rutina JOIN rutina_ejercicio re ON r.id_rutina=re.id_rutina JOIN ejercicio e ON re.id_ejercicio=e.id_ejercicio WHERE ur.id_usuario= %s ', 
+        [session['username']])
+        rutinaEjercicios=cur.fetchall()
+        cur= mysql.connection.cursor()
+        print(rutinaEjercicios)
+        print(rutinas)
+        print("ingreso al template")
+        return render_template('delete_rutina.html', rutinas=rutinas , rutinaEjer=rutinaEjercicios)
+    else:
+        return redirect(url_for('Index'))
+
+@app.route('/home/administracion_rutinas/delete_rutina/confirm_delete', methods= ['POST'])
+def confirm_delete():
+    if request.method=='POST' and 'username' in session:
+        if request.form.get('botonSubmit')=="botonSubmit":
+            cantEjerc= len(request.form.getlist('orden'))
+            idrutina= request.form['numb-rut']
+            usuario= session['username']
+            cur= mysql.connection.cursor()
+            cur.execute('DELETE FROM rutina_ejercicio WHERE id_rutina = {0}'.format(idrutina))
+            cur.execute('DELETE FROM usuario_rutina WHERE id_rutina ={0} and id_usuario={1}'.format(idrutina, usuario))
+            cur.execute('DELETE FROM rutina WHERE id_rutina = {0}'.format(idrutina))
+            mysql.connection.commit()
+            flash('eliminada correctamente')
+        else:
+            flash('No se hicieron cambios')
+        return redirect(url_for('adm_rutinas'))
+    else:
+        return redirect(url_for('Index'))
 
 
 @app.route('/ejercicios')
--- Aplicacion/Base de datos/database.sql
+++ Aplicacion/Base de datos/database.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `aplicaciongym` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
-USE `aplicaciongym`;
 -- MySQL dump 10.13  Distrib 8.0.25, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: aplicaciongym
@@ -104,7 +102,7 @@ CREATE TABLE `rutina` (
   `nombre` varchar(45) DEFAULT NULL,
   `estado` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id_rutina`,`fecha`)
-) ENGINE=InnoDB AUTO_INCREMENT=94 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=104 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -113,7 +111,6 @@ CREATE TABLE `rutina` (
 
 LOCK TABLES `rutina` WRITE;
 /*!40000 ALTER TABLE `rutina` DISABLE KEYS */;
-INSERT INTO `rutina` VALUES (1,'2021-07-24','Abdomen','Creada'),(2,'2021-07-25','Abdomen','Creada'),(3,'2021-10-08','Piernas','Creada'),(33,'2021-10-10','brazos','Creada'),(34,'2021-10-11','Piernas','Creada'),(39,'2021-10-11','prueba','Creada'),(40,'2021-10-11','1','Creada'),(41,'2021-10-11','2','Creada'),(42,'2021-10-11','3','Creada'),(43,'2021-10-11','4','Creada'),(44,'2021-10-11','5','Creada'),(45,'2021-10-11','6','Creada'),(46,'2021-10-11','Natalia','Creada'),(47,'2021-10-11','7','Creada'),(48,'2021-10-11','8','Creada'),(49,'2021-10-11','9','Creada'),(50,'2021-10-11','10','Creada'),(51,'2021-10-11','11','Creada'),(52,'2021-10-11','12','Creada'),(53,'2021-10-11','13','Creada'),(54,'2021-10-11','14','Creada'),(55,'2021-10-11','15','Creada'),(56,'2021-10-11','16','Creada'),(57,'2021-10-11','17','Creada'),(58,'2021-10-11','18','Creada'),(59,'2021-10-11','19','Creada'),(60,'2021-10-11','20','Creada'),(61,'2021-10-11','21','Creada'),(62,'2021-10-11','22','Creada'),(63,'2021-10-11','23','Creada'),(64,'2021-10-11','24','Creada'),(65,'2021-10-11','25','Creada'),(66,'2021-10-11','26','Creada'),(67,'2021-10-11','27','Creada'),(68,'2021-10-11','28','Creada'),(70,'2021-10-11','29','Creada'),(71,'2021-10-11','30','Creada'),(72,'2021-10-11','31','Creada'),(90,'2021-10-11','32','Creada'),(93,'2021-10-23','56','Creada');
 /*!40000 ALTER TABLE `rutina` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -146,7 +143,6 @@ CREATE TABLE `rutina_ejercicio` (
 
 LOCK TABLES `rutina_ejercicio` WRITE;
 /*!40000 ALTER TABLE `rutina_ejercicio` DISABLE KEYS */;
-INSERT INTO `rutina_ejercicio` VALUES ('2021-07-24',1,1,1,1,1,1),('2021-07-24',1,2,1,1,1,1),('2021-07-24',1,5,1,1,1,11),('2021-10-08',3,1,5,15,30,2),('2021-10-08',3,3,3,12,25,3),('2021-10-10',33,1,4,12,11,3),('2021-10-10',33,2,2,15,11,5),('2021-10-11',34,2,3,12,11,3),('2021-10-11',34,3,4,15,15,5),('2021-10-11',90,1,4,12,11,3),('2021-10-11',90,2,2,15,11,5);
 /*!40000 ALTER TABLE `rutina_ejercicio` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -159,13 +155,22 @@ DROP TABLE IF EXISTS `usuario`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `usuario` (
   `id_usuario` int(11) NOT NULL AUTO_INCREMENT,
-  `usuario` varchar(10) DEFAULT NULL,
-  `nombre` varchar(45) DEFAULT NULL,
-  `apellido` varchar(45) DEFAULT NULL,
-  `mail` varchar(45) DEFAULT NULL,
-  `contra` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id_usuario`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+  `usuario` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `nombre` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `apellido` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `mail` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `contra` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `sexo` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `fecha_nacimiento` date NOT NULL,
+  `altura` double DEFAULT NULL,
+  `peso` double DEFAULT NULL,
+  `cintura` double DEFAULT NULL,
+  `cuello` double DEFAULT NULL,
+  `caderas` double DEFAULT NULL,
+  PRIMARY KEY (`id_usuario`),
+  UNIQUE KEY `usuario_UNIQUE` (`usuario`),
+  UNIQUE KEY `mail_UNIQUE` (`mail`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -174,7 +179,7 @@ CREATE TABLE `usuario` (
 
 LOCK TABLES `usuario` WRITE;
 /*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
-INSERT INTO `usuario` VALUES (6,'Danwife','Daniel','Medina','danielmedina012@gmail.com','Alexander1');
+INSERT INTO `usuario` VALUES (4,'Danwife','Daniel','Medina','danielmedina012@gmail.com','Alexander1','Masculino','1998-11-14',176,80,90,40,0);
 /*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -188,14 +193,12 @@ DROP TABLE IF EXISTS `usuario_rutina`;
 CREATE TABLE `usuario_rutina` (
   `id_usuario` int(11) NOT NULL,
   `id_rutina` int(11) NOT NULL,
-  `fecha_rutina` date NOT NULL,
-  PRIMARY KEY (`id_usuario`,`id_rutina`,`fecha_rutina`),
-  KEY `usuario_rutina_rutina_idx` (`fecha_rutina`),
-  KEY `usuario_rutina_rutina_idx1` (`fecha_rutina`,`id_rutina`),
-  KEY `usuario_rutina_rutina_idx2` (`id_rutina`),
-  CONSTRAINT `usuario_rutina_rutina` FOREIGN KEY (`id_rutina`) REFERENCES `rutina` (`id_rutina`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `usuario_rutina_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `fecha` date DEFAULT NULL,
+  PRIMARY KEY (`id_usuario`,`id_rutina`),
+  KEY `usuario_rutina_rutina_fk_idx` (`id_rutina`),
+  CONSTRAINT `usuario_rutina_rutina_fk` FOREIGN KEY (`id_rutina`) REFERENCES `rutina` (`id_rutina`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `usuario_rutina_usuario_fk` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -204,7 +207,6 @@ CREATE TABLE `usuario_rutina` (
 
 LOCK TABLES `usuario_rutina` WRITE;
 /*!40000 ALTER TABLE `usuario_rutina` DISABLE KEYS */;
-INSERT INTO `usuario_rutina` VALUES (6,33,'2021-10-10'),(6,34,'2021-10-11'),(6,39,'2021-10-11'),(6,40,'2021-10-11'),(6,41,'2021-10-11'),(6,42,'2021-10-11'),(6,43,'2021-10-11'),(6,44,'2021-10-11'),(6,45,'2021-10-11'),(6,46,'2021-10-11'),(6,47,'2021-10-11'),(6,48,'2021-10-11'),(6,49,'2021-10-11'),(6,50,'2021-10-11'),(6,51,'2021-10-11'),(6,52,'2021-10-11'),(6,53,'2021-10-11'),(6,54,'2021-10-11'),(6,55,'2021-10-11'),(6,56,'2021-10-11'),(6,57,'2021-10-11'),(6,58,'2021-10-11'),(6,59,'2021-10-11'),(6,60,'2021-10-11'),(6,61,'2021-10-11'),(6,62,'2021-10-11'),(6,63,'2021-10-11'),(6,64,'2021-10-11'),(6,65,'2021-10-11'),(6,66,'2021-10-11'),(6,67,'2021-10-11'),(6,68,'2021-10-11'),(6,70,'2021-10-11'),(6,71,'2021-10-11'),(6,72,'2021-10-11'),(6,93,'2021-10-23');
 /*!40000 ALTER TABLE `usuario_rutina` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -217,4 +219,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-10-23 18:01:56
+-- Dump completed on 2021-11-09 22:33:26
