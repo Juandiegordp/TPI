898881a7534f9da5b6ea4d5b3bc27bfe7d3381ef
Daniel-Mdn <danielmedina012@gmail.com>
2021-11-18 17:37:41 -0300

    ultima version

--- Aplicacion/Presentation/views.py
+++
@@ -1,9 +0,0 @@
-   
-   
-   def register(mysql) 
-   registerForm= forms.RegisterForm(request.form)
-    datos=[]
-    if request.method == 'POST' and registerForm.validate():
-        
-
-    return render_template('register.html', form=registerForm)
--- Aplicacion/forms.py
+++ Aplicacion/forms.py
@@ -1,5 +1,5 @@
 from wtforms import Form, validators, PasswordField, BooleanField, FloatField, IntegerField, RadioField, DateField
-from wtforms import StringField, TextField
+from wtforms import StringField
 from wtforms.fields.html5 import EmailField
 
 class RegisterForm(Form):
@@ -8,16 +8,20 @@ class RegisterForm(Form):
     apellido= StringField('Apellido', [validators.Length(min=4, max=12, message="El apellido que tener entre 3 a 12 caracteres")])
     sexo= RadioField('Sexo', choices =['Masculino', 'Femenino'])
     fechaNacimiento= DateField('Fecha de nacimiento',format='%Y-%m-%d')
-    altura= FloatField('Altura (opcional)', [validators.required()])
-    peso= FloatField('Peso (opcional)', [validators.required()])
-    cintura= FloatField('Cintura (opcional)', [validators.required()])
-    cuello= FloatField('Cuello (opcional)', [validators.required()])
-    caderas= FloatField('Caderas (opcional)')
+    altura= FloatField('Altura en cm (opcional)', [validators.required()])
+    peso= FloatField('Peso en kg (opcional)', [validators.required()])
+    cintura= FloatField('Cintura en cm (opcional)', [validators.required()])
+    cuello= FloatField('Cuello en cm (opcional)', [validators.required()])
+    caderas= FloatField('Caderas en cm (opcional)')
     mail= EmailField('Mail', [validators.Length(min=6, max=35, message="El mail tiene que tener entre 6 a 35 caracteres")])
     contrasenia= PasswordField('Contraseña', [validators.DataRequired(), validators.EqualTo('confirmarContra', message='No coinciden las contraseñas')])
     confirmarContra= PasswordField('Repetir Contraseña')
     terminos = BooleanField('Acepto los terminos y condiciones', [validators.DataRequired(message="Acepte los terminos y condiciones")])
 
 class PerfilForm(Form):
-    usuario= StringField('Usuario', [validators.Length(min=4, max=25)])
+    altura= FloatField('Altura en cm (opcional)', [validators.required()])
+    peso= FloatField('Peso en kg (opcional)', [validators.required()])
+    cintura= FloatField('Cintura en cm (opcional)', [validators.required()])
+    cuello= FloatField('Cuello en cm (opcional)', [validators.required()])
+    caderas= FloatField('Caderas en cm (opcional)')
     
